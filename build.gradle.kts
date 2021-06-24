c340edca005a797a6f9d9f090dae9004e287a8f9
Silvio Giebl <silvio.giebl@hivemq.com>
2021-06-24 15:38:45 +0200

    Update com.hivemq.extension plugin to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,15 @@ group = "com.hivemq.extensions"
 description = "HiveMQ Discovery Extension based on usage of Azure Storage Blobs"
 
 hivemqExtension {
-    name = "Azure Cluster Discovery Extension"
-    author = "HiveMQ"
-    priority = 1000
-    startPriority = 10000
-    sdkVersion = "${property("hivemq-extension-sdk.version")}"
+    name.set("Azure Cluster Discovery Extension")
+    author.set("HiveMQ")
+    priority.set(1000)
+    startPriority.set(10000)
+    sdkVersion.set("${property("hivemq-extension-sdk.version")}")
+
+    resources {
+        from("LICENSE")
+    }
 }
 
 /* ******************** dependencies ******************** */
@@ -27,12 +31,6 @@ dependencies {
     hivemqProvided("ch.qos.logback:logback-classic:${property("logback.version")}")
     implementation("com.azure:azure-storage-blob:${property("azure-storage-blob.version")}")
     implementation("org.aeonbits.owner:owner-java8:${property("owner.version")}")
-}
-
-/* ******************** resources ******************** */
-
-tasks.hivemqExtensionResources {
-    from("LICENSE")
 }
 
 /* ******************** test ******************** */
@@ -54,44 +52,11 @@ tasks.withType<Test> {
 
 /* ******************** integration test ******************** */
 
-sourceSets.create("integrationTest") {
-    compileClasspath += sourceSets.main.get().output
-    runtimeClasspath += sourceSets.main.get().output
-}
-
-val integrationTestImplementation: Configuration by configurations.getting {
-    extendsFrom(configurations.testImplementation.get())
-}
-val integrationTestRuntimeOnly: Configuration by configurations.getting {
-    extendsFrom(configurations.testRuntimeOnly.get())
-}
-
 dependencies {
     integrationTestImplementation("org.testcontainers:testcontainers:${property("testcontainers.version")}")
     integrationTestImplementation("org.testcontainers:toxiproxy:${property("testcontainers.version")}")
     integrationTestImplementation("com.hivemq:hivemq-testcontainer-junit5:${property("hivemq-testcontainer.version")}")
 }
-
-val prepareExtensionTest by tasks.registering(Sync::class) {
-    group = "hivemq extension"
-    description = "Prepares the extension for integration testing."
-
-    from(tasks.hivemqExtensionZip.map { zipTree(it.archiveFile) })
-    into(buildDir.resolve("hivemq-extension-test"))
-}
-
-val integrationTest by tasks.registering(Test::class) {
-    group = "verification"
-    description = "Runs integration tests."
-
-    testClassesDirs = sourceSets[name].output.classesDirs
-    classpath = sourceSets[name].runtimeClasspath
-    shouldRunAfter(tasks.test)
-    dependsOn(prepareExtensionTest)
-}
-
-tasks.check { dependsOn(integrationTest) }
-
 
 /* ******************** checks ******************** */
 
